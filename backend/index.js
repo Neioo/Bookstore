6b2f2db1452a53e27d8b70452eb05cbce09f781b
Shankssu <97654823+Neioo@users.noreply.github.com>
2025-12-01 18:28:40 +0800

    Added files

---
+++ backend/index.js
@@ -0,0 +1,44 @@
+import express from "express";
+import mysql from "mysql2";
+import cors from "cors";
+
+const app = express();
+
+const db = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "1234",
+  database: "test",
+});
+
+// if there is a auth problem
+// alter user root@localhost
+
+app.use(express.json());
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.json("hello this is the backend");
+});
+
+app.get("/books", (req, res) => {
+  const q = "SELECT * FROM books";
+  db.query(q, (data, err) => {
+    if (err) return res.json(err);
+    return res.json(data);
+  });
+});
+
+app.post("/books", (req, res) => {
+  const q = "INSERT INTO books (`title`, `description`,`cover`) VALUES (?)";
+  const values = [req.body.title, req.body.description, req.body.cover];
+
+  db.query(q, [values], (err, data) => {
+    if (err) return res.json(err);
+    return res.json("Book has been created");
+  });
+});
+
+app.listen(8800, () => {
+  console.log("connected to backend!");
+});
